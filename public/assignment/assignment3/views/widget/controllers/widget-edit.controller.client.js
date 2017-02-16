a87ed9ed92e140fee4dd361163284977de9b9d26
ChaosInOrder <qinxuliang2015@gmail.com>
2017-02-15 19:51:14 -0500

    Fix bug

--- public/assignment/assignment3/views/widget/controllers/widget-edit.controller.client.js
+++ public/assignment/assignment3/views/widget/controllers/widget-edit.controller.client.js
@@ -3,31 +3,31 @@
         .module("WebAppMaker")
         .controller("EditWidgetController", EditWidgetController);
 
-    function EditWidgetController($routeParams, $location, WidgetService) {
+    function EditWidgetController($routeParams, $location,$window, WidgetService) {
         var vm = this;
         vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         vm.pageId = $routeParams.pid;
         vm.widgetId = $routeParams.wgid;
 
+
+        vm.updateWidget=updateWidget;
+        vm.deleteWidget=deleteWidget;
         function init() {
             vm.widget = WidgetService.findWidgetById(vm.widgetId);
         }
         init();
 
-        vm.getEditorTemplateUrl = function (type) {
-            return 'views/widget/editor/widget-' + type + '-editor.view.client.html';
-        }
 
-        vm.updateWidget = function () {
+        function updateWidget() {
             WidgetService.updateWidget(vm.widgetId, vm.widget);
-            alert("Widget updated!");
+            window.alert("Update Widget");
             $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
         }
 
-        vm.deleteWidget = function () {
+        function deleteWidget() {
             WidgetService.deleteWidget(vm.widgetId);
-            alert("Widget deleted!");
+            window.alert("Delete Widget!");
             $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
         }
     }
