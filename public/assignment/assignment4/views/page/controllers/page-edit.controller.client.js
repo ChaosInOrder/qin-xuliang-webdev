4a2f3c2d8a661888b0912e3e89a739dfb5ffd050
ChaosInOrder <qinxuliang2015@gmail.com>
2017-03-01 12:34:45 -0500

    Finish page

--- public/assignment/assignment4/views/page/controllers/page-edit.controller.client.js
+++ public/assignment/assignment4/views/page/controllers/page-edit.controller.client.js
@@ -12,22 +12,41 @@
         vm.deletePage=deletePage;
         vm.updatePage=updatePage;
         function init() {
-            vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
-            vm.page=PageService.findPageById(vm.pageId);
+            PageService
+                .findPageById(vm.pageId)
+                .success(function (page) {
+                    vm.page=page;
+                });
         }
         init();
 
         function deletePage() {
-            PageService.deletePage(vm.pageId);
-            $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
             window.alert("Delete Page!");
+            var answer = confirm("Are you sure?");
+            console.log(answer);
+            if(answer) {
+                PageService
+                    .deletePage(vm.pageId)
+                    .success(function () {
+                        $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+                    })
+                    .error(function () {
+                        $window.alert("Unable to delete the page!");
+                    });
+            }
 
         }
         function updatePage(page) {
-            PageService.updatePage(vm.pageId,page);
-            $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
-            window.alert("Update Page Info!");
-
+            PageService
+                .updatePage(vm.pageId,page)
+                .success(function (page) {
+                    $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+                    $window.alert("Update the page!");
+                })
+                .error(function (page) {
+                    $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+                    $window.alert("Unable to change the page!");
+                });
         }
 
     }
