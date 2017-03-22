3b4fc02e93a940db522456d60b1c34e69f6edf0c
ChaosInOrder <qinxuliang2015@gmail.com>
2017-03-21 21:41:06 -0400

    Finish widget data base

--- assignment/model/widget/widget.model.server.js
+++ assignment/model/widget/widget.model.server.js
@@ -18,6 +18,7 @@ module.exports=function () {
     return api;
     function createWidget(pageId,widget) {
         widget._page=pageId;
+        widget.size=1
         return widgetModel.find({_page:pageId})
             .then(function (widgets) {
                 widget.index=widgets.length;
@@ -36,43 +37,58 @@ module.exports=function () {
         return widgetModel.update({_id:widgetId}, {$set: widget})
     }
     function deleteWidget(widgetId) {
-        return widgetModel.findWidgetById(widgetId)
+        return widgetModel.findOne({_id:widgetId})
             .then(function (widget) {
-                return widgetModel.findAllWidgetsForPage(widget._page)
-                    .then(function (widgets) {
-                        return widgetModel.reorderWidget(widget._page,widget.index,widgets.length)
-                            .then(function (status) {
-                                widget.remove();
-                            },function (err) {
-                                console.log(err)
-                            })
-                    })
-
+                return widgetModel.find({_page:widget._page},function (err,widgets) {
+                    // console.log("origin delete",widget)
+                    moveUp(widgets,widget.index,widgets.length);
+                    // console.log("after move up",widgets)
+                    widget.remove()
+                })
             });
 
     }
     function reorderWidget(pageId,start,end) {
-        return widgetModel.find({_page:pageId})
-            .then(function (widgets) {
-                for(var i in widgets){
-                    if (initial < final && widgets[i].index > initial && widgets[i].index <= final && widgets[i].pageId == pageId) {
-                        widgets[i].index -= 1;
-                        widgets[i].save()
-                    }
+        var s=parseInt(start);
+        var f=parseInt(end);
+        return widgetModel.find({_page:pageId},function (err,widgets) {
+            if(s>f){moveDown(widgets,s,f)}
+            else if (s<f){moveUp(widgets,s,f)}
+        });
 
-                    else if(initial > final && widgets[i].index < initial && widgets[i].index >= final && widgets[i].pageId == pageId) {
-                        widgets[i].index += 1;
-                        widgets[i].save();
-                    }
 
-                    else if (widgets[i].index == initial && widgets[i].pageId == pageId) {
-                        widgets[i].index = parseInt(final);
-                        widgets[i].save();
-                    }
-                }
-            },function (err) {
+    }
+    function moveUp(widgets,s,f) {
+        for(var i in widgets){
+            if(widgets[i].index>s && widgets[i].index<=f){
+                widgets[i].index -= 1;
+                widgets[i].save();
+                // console.log("move up",widgets[i]);
+            }
+            else if(widgets[i].index==s){
+                // console.log("final position",f)
+                widgets[i].index=f;
+                widgets[i].save();
+                // console.log("delete widget after save",widgets[i])
+            }
+        }
 
-            })
+    }
+
+    function moveDown(widgets,s,f) {
+        for(var i in widgets)
+        {
+            if(widgets[i].index<s && widgets[i].index>=f)
+            {
+                widgets[i].index += 1;
+                widgets[i].save();
+            }
+            else if(widgets[i].index==s)
+            {
+                widgets[i].index=f;
+                widgets[i].save();
+            }
+        }
 
     }
 }
